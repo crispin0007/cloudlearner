0b4307e61deee83f92a3bc4c0b1d737f9ecb7d8b
Lalit Chaudhary <lalitchaudhary622@gmail.com>
2023-08-09 10:49:19 +0545

    nav

--- aboutus.php
+++ aboutus.php
@@ -4,9 +4,9 @@
 <head>
 
     <title>About Us | CloudLearner</title>
-    <?php
-    include 'elements/topheader.php'
-        ?>
+    <?php 
+	include 'elements/topheader.php'
+	?>
 </head>
 
 <body>
@@ -15,9 +15,9 @@
         <div class="loader"></div>
     </div>
     <!-- Header section -->
-    <?php
-    include 'elements/navbar.php'
-        ?>
+    <?php 
+	include 'elements/navbar.php'
+	?>
     <!-- Header section end -->
     <!-- Page info -->
     <div class="page-info-section set-bg" data-setbg="img/page-bg/4.jpg">
@@ -40,7 +40,7 @@
                 </div>
             </div>
             <div class="row">
-                <!-- column  -->
+                <!-- column  -->c
                 <div class="col-lg-4 mb-4">
                     <!-- Row -->
                     <div class="row">
@@ -51,12 +51,43 @@
                         <div class="col-md-12 text-center">
                             <div class="pt-2">
                                 <h5 class="mt-4 font-weight-medium mb-0">Sudip Sharma</h5>
-                                <h6 class="subtitle mb-3">Developer </h6>
-                                <p>the frontend developer stands as a creative visionary. Skillfully wielding HTML, CSS,
-                                    and JavaScript, they craft captivating user interfaces,
-                                    translating design concepts into interactive realities.
-                                    Their code expertise and design sensibilities combine to elevate user experiences
-                                    and breathe life into digital landscapes.</p>
+                                <h6 class="subtitle mb-3">Mesource Manager</h6>
+                                <p>You can relay on our amazing features list and also our customer services will be
+                                    great experience.</p>
+                                <ul class="list-inline">
+                                    <li class="list-inline-item"><a href="#"
+                                            class="text-decoration-none d-block px-1"><i
+                                                class="icon-social-facebook"></i></a></li>
+                                    <li class="list-inline-item"><a href="#"
+                                            class="text-decoration-none d-block px-1"><i
+                                                class="icon-social-twitter"></i></a></li>
+                                    <li class="list-inline-item"><a href="#"
+                                            class="text-decoration-none d-block px-1"><i
+                                                class="icon-social-instagram"></i></a></li>
+                                    <li class="list-inline-item"><a href="#"
+                                            class="text-decoration-none d-block px-1"><i
+                                                class="icon-social-behance"></i></a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- Row -->
+                </div>
+                <!-- column  -->
+                <!-- column  -->    
+                <div class="col-lg-4 mb-4">
+                    <!-- Row -->
+                    <div class="row">
+                        <div class="col-md-12">
+                            <img src="https://www.wrappixel.com/demos/ui-kit/wrapkit/assets/images/team/t1.jpg"
+                                alt="wrapkit" class="img-fluid rounded-circle" />
+                        </div>
+                        <div class="col-md-12 text-center">
+                            <div class="pt-2">
+                                <h5 class="mt-4 font-weight-medium mb-0">Lalit Chaudhary</h5>
+                                <h6 class="subtitle mb-3">Backend Developer</h6>
+                                <p>You can relay on our amazing features list and also our customer services will be
+                                    great experience.</p>
                                 <ul class="list-inline">
                                     <li class="list-inline-item"><a href="#"
                                             class="text-decoration-none d-block px-1"><i
@@ -87,47 +118,10 @@
                         </div>
                         <div class="col-md-12 text-center">
                             <div class="pt-2">
-                                <h5 class="mt-4 font-weight-medium mb-0">Lalit chaudhary</h5>
-                                <h6 class="subtitle mb-3"> Developer </h6>
-                                <p> A promising pro developer student, wields expertise with precision. Fueled by
-                                    ambition, he pioneers intricate solutions,
-                                    scripting success in the digital realm with skillful mastery.</p>
-                                <ul class="list-inline">
-                                    <li class="list-inline-item"><a href="#"
-                                            class="text-decoration-none d-block px-1"><i
-                                                class="icon-social-facebook"></i></a></li>
-                                    <li class="list-inline-item"><a href="#"
-                                            class="text-decoration-none d-block px-1"><i
-                                                class="icon-social-twitter"></i></a></li>
-                                    <li class="list-inline-item"><a href="#"
-                                            class="text-decoration-none d-block px-1"><i
-                                                class="icon-social-instagram"></i></a></li>
-                                    <li class="list-inline-item"><a href="#"
-                                            class="text-decoration-none d-block px-1"><i
-                                                class="icon-social-behance"></i></a></li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-                    <!-- Row -->
-                </div>
-                <!-- column  -->
-                <!-- column  -->
-                <div class="col-lg-4 mb-4">
-                    <!-- Row -->
-                    <div class="row">
-                        <div class="col-md-12">
-                            <img src="https://www.wrappixel.com/demos/ui-kit/wrapkit/assets/images/team/t1.jpg"
-                                alt="wrapkit" class="img-fluid rounded-circle" />
-                        </div>
-                        <div class="col-md-12 text-center">
-                            <div class="pt-2">
-                                <h5 class="mt-4 font-weight-medium mb-0">Solon timalsina</h5>
-                                <h6 class="subtitle mb-3">UI/UX</h6>
-                                <p>budding Ui/UX designer, melds creativity and empathy to envision user-centric
-                                    interfaces.
-                                    With an eye for aesthetics and a passion for seamless experiences, he shapes digital
-                                    journeys.</p>
+                                <h5 class="mt-4 font-weight-medium mb-0">Solon Timalsina</h5>
+                                <h6 class="subtitle mb-3">Frontend Developer</h6>
+                                <p>You can relay on our amazing features list and also our customer services will be
+                                    great experience.</p>
                                 <ul class="list-inline">
                                     <li class="list-inline-item"><a href="#"
                                             class="text-decoration-none d-block px-1"><i
@@ -159,11 +153,9 @@
                         <div class="col-md-12 text-center">
                             <div class="pt-2">
                                 <h5 class="mt-4 font-weight-medium mb-0">Sabina Mahato</h5>
-                                <h6 class="subtitle mb-3">UI/UX</h6>
-                                <p>budding Ui/UX designer, melds creativity and empathy to envision user-centric
-                                    interfaces.
-                                    With an eye for aesthetics and a passion for seamless experiences, he shapes digital
-                                    journeys.</p>
+                                <h6 class="subtitle mb-3">Hosting Manager</h6>
+                                <p>You can relay on our amazing features list and also our customer services will be
+                                    great experience.</p>
                                 <ul class="list-inline">
                                     <li class="list-inline-item"><a href="#"
                                             class="text-decoration-none d-block px-1"><i
@@ -194,11 +186,10 @@
                         </div>
                         <div class="col-md-12 text-center">
                             <div class="pt-2">
-                                <h5 class="mt-4 font-weight-medium mb-0">Rabina lamichhane</h5>
-                                <h6 class="subtitle mb-3">Developer</h6>
-                                <p>A dedicated developer student, dives into code's realm, crafting solutions with
-                                    innovation.
-                                    Curious and persistent, he forges the future one line at a time.</p>
+                                <h5 class="mt-4 font-weight-medium mb-0">Rabina Lamichhane</h5>
+                                <h6 class="subtitle mb-3">Hosting Manager</h6>
+                                <p>You can relay on our amazing features list and also our customer services will be
+                                    great experience.</p>
                                 <ul class="list-inline">
                                     <li class="list-inline-item"><a href="#"
                                             class="text-decoration-none d-block px-1"><i
@@ -229,11 +220,10 @@
                         </div>
                         <div class="col-md-12 text-center">
                             <div class="pt-2">
-                                <h5 class="mt-4 font-weight-medium mb-0">yanki tamang</h5>
-                                <h6 class="subtitle mb-3">Developer</h6>
-                                <p>A dedicated developer student, dives into code's realm, crafting solutions with
-                                    innovation.
-                                    Curious and persistent, he forges the future one line at a time.</p>
+                                <h5 class="mt-4 font-weight-medium mb-0">Yanki Tamang</h5>
+                                <h6 class="subtitle mb-3">Frontend Developer</h6>
+                                <p>You can relay on our amazing features list and also our customer services will be
+                                    great experience.</p>
                                 <ul class="list-inline">
                                     <li class="list-inline-item"><a href="#"
                                             class="text-decoration-none d-block px-1"><i
@@ -258,9 +248,9 @@
     </div>
     <!-- team member display end -->
 
-    <?php
-    include 'elements/footer.php'
-        ?>
+    <?php 
+	include 'elements/footer.php'
+	?>
 </body>
 
 </html>
